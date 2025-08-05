be6504d9ca704874b52d94debf10d732f5359296
MElisoltanov <elisoltanov.moussa@outlook.fr>
2025-08-05 13:53:35 +0200

    Implement binary_tree_is_leaf function to check if a node is a leaf

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,32 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - Checks if a node is a leaf
+ * @node: Pointer to the node to check
+ *
+ * Return: 1 if node is a leaf, 0 otherwise
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	/* Étape 1 : Vérifie si le pointeur node est NULL. */
+	/* Retourne 0 si c’est le cas. */
+
+	if (node == NULL)
+	{
+		return (0);
+	}
+
+	/* Étape 2 : Vérifie si le nœud a des enfants. */
+		/* - S’il a un enfant gauche ou droit, retourne 0 */
+		/* - Sinon, c’est une feuille → retourne 1 */
+
+	if (node->right == NULL && node->left == NULL)
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+}
